b19c5216e6234878fb4403588ece50696e5364c1
alexdiffo <diffalex@gmail.com>
2023-01-31 14:26:45 +0100

    modifying index path

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/herokudeploy'));
+app.use(express.static('./herokudeploy'));
 
 app.get('/*', (req, res) => {
-    res.sendFile('index.html', {root: 'dist/herokudeploy/src'})
+    res.sendFile('index.html', {root: 'herokudeploy/src'})
 });
 
 // Start the app by listening on the default Heroku port
