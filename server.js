e60bdcaa68595b9e7ade04c1699f963f85a4a9ae
alexdiffo <diffalex@gmail.com>
2023-01-31 14:09:42 +0100

    modifying index path

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.static('./dist/herokudeploy'));
 
 app.get('/*', (req, res) => {
-    res.sendFile('index.html', {root: 'dist/herokudeploy/src/index.html'})
+    res.sendFile('index.html', {root: 'dist/herokudeploy/src'})
 });
 
 // Start the app by listening on the default Heroku port
